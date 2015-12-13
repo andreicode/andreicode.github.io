9f78c4b66adf7b43564adeb4850b3634266789a0
andreicode <andreipopa.1992@yahoo.com>
2015-12-13 06:29:56 +0200

    'ahh2'

---
+++ scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bibliotecaMetodicaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bibliotecaMetodicaApp
+ */
+angular.module('bibliotecaMetodicaApp')
+  .controller('MainCtrl', function () {
+  });
---
+++ scripts/controllers/view.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bibliotecaMetodicaApp.controller:AboutCtrl
+ * @description
+ * # viewCtrl
+ * Controller of the bibliotecaMetodicaApp
+ */
+angular.module('bibliotecaMetodicaApp')
+  .controller('viewCtrl', function () {
+  });
